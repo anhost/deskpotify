5d1b09ef63b373fc8dd581f463a35c64dcdf9bfd
anhost <an.host@ya.ru>
2023-02-14 13:00:44 +0300

    Add files via upload
    Init

---
+++ deskpotify.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+var menuItem = chrome.contextMenus.create({
+		"id": "anhspsrch",
+		"title": "Deskpotify: '%s'",
+		"contexts":["selection"]
+	});
+	
+function searchOnClick(info, tab){
+	var targetURL = "spotify:search:" + info.selectionText;
+	chrome.tabs.update(tab.id,{"url":targetURL});
+}
+
+chrome.contextMenus.onClicked.addListener(searchOnClick);
+
